9ff7f42146981406f6383a2c8757007b9783f162
Call Vin <kanggara75@gmail.com>
2025-07-05 08:33:35 +0700

    remove bun

--- src/page.js
+++ src/page.js
@@ -1,11 +1,11 @@
-import { parse } from 'node-html-parser';
+import { parse } from "node-html-parser";
 
 export default class Page {
 	constructor(page) {
 		this.response = this.init(page);
 	}
 	async init(page = 1) {
-		const rest = await fetch('https://www.e-ipo.co.id/id/ipo/index?page=' + page + '&per-page=12&view=list');
+		const rest = await fetch("https://www.e-ipo.co.id/id/ipo/index?page=" + page + "&per-page=12&view=list");
 		return await rest.text();
 	}
 
@@ -13,10 +13,9 @@ export default class Page {
 		try {
 			const document = parse(await this.response);
 			const pageList = document
-				.querySelector('#w0 > div.row.margin-top20 > div.col-md-7 > ul')
-				.getElementsByTagName('li');
-			const lastPage = pageList[pageList.length - 2].firstChild.textContent;
-			return lastPage;
+				.querySelector("#w0 > div.row.margin-top20 > div.col-md-7 > ul")
+				.getElementsByTagName("li");
+			return pageList[pageList.length - 2].firstChild.textContent;
 		} catch (error) {
 			console.error(error);
 			return 1;
